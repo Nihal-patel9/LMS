fe038a3fbcb92c5b3f1444bbde751d9c6d44be85
Nihal-patel9 <1999nihalpatel@gmail.com>
2024-06-26 14:30:01 +0530

    Added eslint

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-router-dom'
 import store from './Redux/store.js'
+import { Toaster } from 'react-hot-toast';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store ={store}>
