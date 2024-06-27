ecd7ec4d24131e59552f147062f661192afca005
Nihal-patel9 <1999nihalpatel@gmail.com>
2024-06-27 12:58:24 +0530

    added footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import { Routes } from 'react-router-dom'
 import './App.css'
+import Footer from './Components/Footer';
 
 function App() {
-  
 
   return (
     <>
-      <Routes>
-        { /* <Route path="/" element={<Home />} > </Route> */}
-
-      </Routes>
-        
+      { /* <Routes> */}
+      { /* <Route path="/" element= { <Home /> } > </Route> */}
+      
+      { /* </Routes> */}
+     
+      
     </>
+
   )
 }
 
-export default App
+export default App;
+  
+
+  
+  
--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import './index.css'
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-dom/client'
-import { Provider } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux';
 import { Toaster } from 'react-hot-toast';
 
 import App from './App.jsx'
@@ -13,7 +13,7 @@ import store from './Redux/store.js'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <Provider store ={store}>
+    <Provider store ={store} >
       <BrowserRouter>
         <App />
         <Toaster />
