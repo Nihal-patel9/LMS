48fae74b6f4ca2da26807cee84c64c40ee2f1233
Nihal-patel9 <1999nihalpatel@gmail.com>
2024-07-05 14:42:31 +0530

    Added AboutUs Page with carousel

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -41,12 +41,12 @@ function HomeLayout({ children }) {
 
 
     return (
-        <div className="min-h-[90vh]">
+       <div className="min-h-[90vh]">
             <div className="drawer absolute left-0 z-50 w-fit">
                 <input className="drawer-toggle" id="my-drawer" type="checkbox" />
                 <div className="drawer-content">
                     <label htmlFor="my-drawer" className="cursor-pointer relative">
-                         <FiMenu 
+                        <FiMenu 
                             onClick={changeWidth}
                             size={"32px"}
                             className="font-bold text-white m-4"
@@ -57,13 +57,12 @@ function HomeLayout({ children }) {
                     <label htmlFor="my-drawer" className="drawer-overlay">
                     </label>
                     <ul className="menu p-4 w-48 h-[100%] sm:w-80 bg-base-200 text-base-content relative">
-                         <li className="w-fit absolute right-2 z-50">
+                        <li className="w-fit absolute right-2 z-50">
                             <button onClick={hideDrawer}>
                                 <AiFillCloseCircle size={24} />
                             </button>
                         </li>
-
-                         <li>
+                        <li>
                             <Link to="/">Home</Link>
                         </li>
 
@@ -72,18 +71,17 @@ function HomeLayout({ children }) {
                                 <Link to="/admin/dashboard"> Admin DashBoard</Link>
                             </li>
                         )}
-
                         {isLoggedIn && role === 'ADMIN' && (
                             <li>
                                 <Link to="/course/create"> Create new course</Link>
                             </li>
                         )}
 
-                         <li>
-                                <Link to="/courses"> All Course</Link>
+                        <li>
+                            <Link to="/courses">All Courses</Link>
                         </li>
-                        
-                         <li>
+
+                        <li>
                             <Link to="/contact">Contact Us</Link>
                         </li>
 
@@ -103,8 +101,8 @@ function HomeLayout({ children }) {
                                 </div>
                             </li>
                         )}
-                        
-                         {isLoggedIn && (
+
+                        {isLoggedIn && (
                             <li className="absolute bottom-4 w-[90%]">
                                 <div className="w-full flex items-center justify-center">
                                     <button className='btn-primary px-4 py-1 font-semibold rounded-md w-full'>
@@ -120,13 +118,12 @@ function HomeLayout({ children }) {
                 </div>
             </div>
 
-             { children }
+            { children }
 
-             <Footer />
- 
+            <Footer />
         </div>
-
     );
+
 }
 
 export default HomeLayout;
